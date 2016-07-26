3e7fc3a8d6e222acfab8f6df8513adea91deb28e
Kosy Anyanwu <kosy.anyanwu@andela.com>
2016-07-26 22:07:26 +0100

    Add playbook and task to set up doc cache app on ec2

--- monitoring_deployments_9/features/step_definitions/cloudformation_steps.rb
+++ monitoring_deployments_9/features/step_definitions/cloudformation_steps.rb
@@ -11,8 +11,8 @@ Then(/^two instances should be created$/) do
   cmd = "aws cloudformation list-stack-resources --stack-name EC2 --region us-west-2"
   output, _, _ = Open3.capture3 "#{cmd}"
   output = JSON.parse(output)
+
   summary = output["StackResourceSummaries"]
   expect(summary.size).to eq(2)
-  expect(summary.first['ResourceType']).to match("AWS::EC2::Instance")
-  expect(summary.last['ResourceType']).to match("AWS::EC2::Instance")
+  expect(summary.all? { |r| r["ResourceType"] == "AWS::EC2::Instance" }).to be true
 end
